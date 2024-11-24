8b9d52c899efa0f5da26441535579f0729f2455c
Caio Vinícius <devcaioviniciusml@gmail.com>
2024-11-24 03:12:07 -0300

    style: delete one line

--- src/services/geminiService.js
+++ src/services/geminiService.js
@@ -2,7 +2,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-
+  
 export async function generateDesc(imageBuffer) {
   const prompt = "Gere uma descrição em português do Brasil para a seguinte Imagem";
 
